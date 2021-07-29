ba1b8c37332ac3e8121ae18a1e732a73c487429b
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-28 23:43:05 -0300

    fixed authentication error in hook

--- src/hooks/useIsAuthenticated.js
+++ src/hooks/useIsAuthenticated.js
@@ -1,13 +1,31 @@
+import { logout } from "actions/userActions";
 import { useEffect } from "react"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 
 export const useIsAuthenticated = () => {
 
 
-    const isAuthenticated = useSelector(state => state.userLogin.userInfo !== undefined ? state.userLogin.userInfo : null)
+    const isAuthenticated = useSelector(function (state) {
+        if (state === undefined || state === null) {
+            console.log({ case: 1, state })
+            return null
+        }
+        if (typeof (state) === "object") {
+            if (state?.userLogin?.userInfo && Object.keys(state?.userLogin?.userInfo).length == 0) {
+                // console.log({ case: 2, state })
+                return null
+            }
+            if (state.userLogin.userInfo !== undefined) {
+                // console.log({ case: 3, state })
+                return state.userLogin.userInfo
+            }
+        }
+        console.log({ state })
+    })
     const history = useHistory();
+    const dispatch = useDispatch()
 
     useEffect(() => {
 
@@ -31,5 +49,11 @@ export const useIsAuthenticated = () => {
 
     }, [history.location, isAuthenticated])
 
+    useEffect(() => {
+        if (new Date() > new Date(isAuthenticated?.expires_at)) {
+            dispatch(logout())
+        }
+        console.log(isAuthenticated?.expires_at)
+    })
 
 }
