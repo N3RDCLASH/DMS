a6c18fc523bb5eb48d4496611c04e34fe2be91ac
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-28 23:44:33 -0300

    updated login form

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -18,8 +18,8 @@ import Email from "@material-ui/icons/Email";
 import Lock from "@material-ui/icons/Lock";
 import ClipLoader from "react-spinners/ClipLoader";
 import { useDispatch, useSelector } from "react-redux";
-
-
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
 
 // core components
 import componentStyles from "assets/theme/views/auth/login.js";
@@ -42,13 +42,16 @@ function Login() {
   const userLogin = useSelector((state) => state.userLogin);
   const history = useHistory()
   const { loading, error, userInfo } = userLogin;
-
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const override = css`
   display: block;
   margin: 0 auto;
   border-color: #5e72e4;
 `;
-
+  let schema = yup.object().shape({
+    email: yup.string().required('Voer email in.').trim(' '),
+    password: yup.string().required('Voer password in.')
+  });
   useEffect(() => {
     return () => {
 
@@ -56,7 +59,7 @@ function Login() {
   }, [loading, error, userInfo])
 
   const submitForm = () => {
-    dispatch(login(email, password))
+    console.log(dispatch(login(email, password)))
   }
   return (
     <>
@@ -81,7 +84,7 @@ function Login() {
               } width={100} height="100" src={require("assets/img/icons/qualogy.svg").default} alt="" />
             </Box>
             {loading ? <ClipLoader loading={loading} css={override} size={60} /> :
-              <form action="" >
+              <form action="" onSubmit={handleSubmit(submitForm)}>
                 <FormControl
                   variant="filled"
                   component={Box}
@@ -93,6 +96,7 @@ function Login() {
                     type="email"
                     placeholder="Email"
                     onInput={({ target: { value } }) => setEmail(value)}
+                    required
                     startAdornment={
                       <InputAdornment position="start">
                         <Email />
@@ -111,6 +115,7 @@ function Login() {
                     type="password"
                     placeholder="Password"
                     onInput={({ target: { value } }) => setPassword(value)}
+                    required
                     startAdornment={
                       <InputAdornment position="start">
                         <Lock />
@@ -129,7 +134,7 @@ function Login() {
                   }}
                 />
                 <Box textAlign="center" marginTop="1.5rem" marginBottom="1.5rem">
-                  <Button color="primary" variant="contained" onClick={submitForm}>
+                  <Button type="submit" color="primary" variant="contained" >
                     Sign in
                   </Button>
                 </Box>
