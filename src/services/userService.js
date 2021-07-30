c7df0413cc26b89381bf4e9739d46fd959f63937
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-07-29 23:32:52 -0300

    added fetchUsers and updateUser methods to service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { apiUrl } from 'index';
 
-export const fetchAllUsers = async ({ queryKey }) => {
+export const fetchUsers = async ({ queryKey }) => {
     const token = queryKey[1]
     const config = {
         headers: {
@@ -13,5 +13,33 @@ export const fetchAllUsers = async ({ queryKey }) => {
         const { data } = await axios.get(`${apiUrl}/users`, config)
         return data
     } catch (error) { console.log(error) }
+}
+
+export const fetchUser = async ({ queryKey }) => {
+    const token = queryKey[1]
+    const id = queryKey[2]
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        const { data } = await axios.get(`${apiUrl}/users/${id}`, config)
+        return data
+    } catch (error) { console.log(error) }
 
 }
+export const updateUser = async ({ user, token, id }) => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        await axios.put(`${apiUrl}/users/${id}`, user, config)
+    } catch (error) { console.log(error) }
+
+}
+
