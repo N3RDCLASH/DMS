a168d97bdfd495ab0ba94cf6186ac6ce30af866e
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-01 02:34:06 -0300

    added role service

---
+++ src/services/roleService.js
@@ -0,0 +1,106 @@
+import axios from 'axios'
+import { apiUrl } from 'index';
+
+export const createRole = async ({ role, token }) => {
+
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        await axios.post(`${apiUrl}/roles`, role, config)
+    } catch (error) { console.log(error) }
+}
+
+export const fetchRoles = async ({ queryKey }) => {
+    const token = queryKey[1]
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        const { data } = await axios.get(`${apiUrl}/roles`, config)
+        return data
+    } catch (error) { console.log(error) }
+}
+
+export const fetchRole = async ({ queryKey }) => {
+    const token = queryKey[1]
+    const id = queryKey[2]
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        const { data } = await axios.get(`${apiUrl}/roles/${id}`, config)
+        return data
+    } catch (error) { console.log(error) }
+
+}
+
+export const updateRole = async ({ user, token, id }) => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        await axios.put(`${apiUrl}/roles/${id}`, user, config)
+    } catch (error) { console.log(error) }
+
+}
+
+export const deleteRole = async ({ token, id }) => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    try {
+        await axios.delete(`${apiUrl}/roles/${id}`, config)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const addPermissionToRole = async ({ permission, token, id }) => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    try {
+
+        await axios.post(`${apiUrl}/roles/${id}`, permission, config)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const removePermissionFromRole = async ({ permission, token, id }) => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    try {
+        await axios.delete(`${apiUrl}/roles/${id}/permission`, { ...config, data: { ...permission } })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
