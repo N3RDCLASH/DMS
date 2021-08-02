b23920b79ab443d1a24b42be7297c6423cbf2380
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-02 02:42:35 -0300

    Updated Routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 // core components
 import Dashboard from "views/admin/Dashboard.js";
-import Icons from "views/admin/Icons.js";
+// import Icons from "views/admin/Icons.js";
 import Login from "views/auth/Login.js";
-import Maps from "views/admin/Maps.js";
+// import Maps from "views/admin/Maps.js";
 import Profile from "views/admin/Profile.js";
 import Users from "views/admin/Users/Users.js";
 // import Register from "views/auth/Register.js";
@@ -10,24 +10,25 @@ import Users from "views/admin/Users/Users.js";
 // @material-ui/icons components
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
-import CloudUpload from "@material-ui/icons/CloudUpload";
+// import CloudUpload from "@material-ui/icons/CloudUpload";
 
 // import Dns from "@material-ui/icons/Dns";
 // import FlashOn from "@material-ui/icons/FlashOn";
 // import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
-import Grain from "@material-ui/icons/Grain";
-import LocationOn from "@material-ui/icons/LocationOn";
+// import Grain from "@material-ui/icons/Grain";
+// import LocationOn from "@material-ui/icons/LocationOn";
 // import Palette from "@material-ui/icons/Palette";
 import Person from "@material-ui/icons/Person";
 import Tv from "@material-ui/icons/Tv";
 import VpnKey from "@material-ui/icons/VpnKey";
-import FileUpload from "views/admin/FileUpload";
+// import FileUpload from "views/admin/FileUpload";
 import SingleUser from "views/admin/Users/SingleUser";
 import SingleRole from "views/admin/Roles/SingleRole";
 import Roles from "views/admin/Roles/Roles";
-import { Security } from "@material-ui/icons";
+import { InsertDriveFile, Security } from "@material-ui/icons";
 import SinglePermission from "views/admin/Permissions/SinglePermission";
 import Permissions from "views/admin/Permissions/Permissions";
+import Documents from "views/admin/Documents/Documents";
 
 var routes = [
 
@@ -35,32 +36,32 @@ var routes = [
     path: "/home",
     name: "Dashboard",
     icon: Tv,
-    iconColor: "Primary",
+    iconColor: "PrimaryLight",
     component: Dashboard,
     layout: "/app",
     sidebar: true
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: Grain,
-    iconColor: "Primary",
-    component: Icons,
-    layout: "/app",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: LocationOn,
-    iconColor: "Warning",
-    component: Maps,
-    layout: "/app",
-  },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: Grain,
+  //   iconColor: "ErrorLightLight",
+  //   component: Icons,
+  //   layout: "/app",
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: LocationOn,
+  //   iconColor: "ErrorLight",
+  //   component: Maps,
+  //   layout: "/app",
+  // },
   {
     path: "/profile",
     name: "User Profile",
     icon: Person,
-    iconColor: "WarningLight",
+    iconColor: "ErrorLight",
     component: Profile,
     layout: "/app",
   },
@@ -68,7 +69,7 @@ var routes = [
   //   path: "/tables",
   //   name: "Tables",
   //   icon: FormatListBulleted,
-  //   iconColor: "Error",
+  //   iconColor: "ErrorLight",
   //   component: Tables,
   //   layout: "/app",
   // },
@@ -84,16 +85,16 @@ var routes = [
   //   path: "/register",
   //   name: "Register",
   //   icon: AccountCircle,
-  //   iconColor: "ErrorLight",
+  //   iconColor: "ErrorLightLight",
   //   component: Register,
   //   layout: "/auth",
   // },
   {
-    path: "/upload",
-    name: "Upload",
-    icon: CloudUpload,
-    iconColor: "success",
-    component: FileUpload,
+    path: "/documents",
+    name: "Documents",
+    icon: InsertDriveFile,
+    iconColor: "ErrorLight",
+    component: Documents,
     layout: "/app",
     sidebar: true
   },
@@ -101,7 +102,7 @@ var routes = [
     path: "/users",
     name: "Users",
     icon: AccountCircle,
-    iconColor: "success",
+    iconColor: "PrimaryLight",
     component: Users,
     layout: "/app",
     sidebar: true,
@@ -110,7 +111,7 @@ var routes = [
   {
     path: "/users/:id",
     name: "Single User",
-    iconColor: "success",
+    iconColor: "ErrorLight",
     component: SingleUser,
     layout: "/app",
     // sidebar: true
@@ -119,7 +120,7 @@ var routes = [
     path: "/roles",
     name: "Roles",
     icon: SupervisedUserCircleIcon,
-    iconColor: "success",
+    iconColor: "ErrorLight",
     component: Roles,
     layout: "/app",
     sidebar: true,
@@ -128,7 +129,7 @@ var routes = [
   {
     path: "/roles/:id",
     name: "Single Role",
-    iconColor: "success",
+    iconColor: "ErrorLight",
     component: SingleRole,
     layout: "/app",
   },
@@ -136,7 +137,7 @@ var routes = [
     path: "/permissions",
     name: "Permissions",
     icon: Security,
-    iconColor: "success",
+    iconColor: "PrimaryLight",
     component: Permissions,
     layout: "/app",
     sidebar: true,
@@ -145,7 +146,7 @@ var routes = [
   {
     path: "/permissions/:id",
     name: "Single Permission",
-    iconColor: "success",
+    iconColor: "PrimaryLight",
     component: SinglePermission,
     layout: "/app",
   },
