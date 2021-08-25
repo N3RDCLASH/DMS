f10902a813c16e07302a0cd42c25761b6b8ec9a2
Joel Naarendorp <joel.naarendorp.natin@gmail.com>
2021-08-25 12:52:35 -0300

    removed console.log from hook

--- src/hooks/useIsAuthenticated.js
+++ src/hooks/useIsAuthenticated.js
@@ -9,20 +9,16 @@ export const useIsAuthenticated = () => {
 
     const isAuthenticated = useSelector(function (state) {
         if (state === undefined || state === null) {
-            console.log({ case: 1, state })
             return null
         }
         if (typeof (state) === "object") {
             if (state?.userLogin?.userInfo && Object.keys(state?.userLogin?.userInfo).length == 0) {
-                // console.log({ case: 2, state })
                 return null
             }
             if (state.userLogin.userInfo !== undefined) {
-                // console.log({ case: 3, state })
                 return state.userLogin.userInfo
             }
         }
-        console.log({ state })
     })
     const history = useHistory();
     const dispatch = useDispatch()
@@ -53,7 +49,6 @@ export const useIsAuthenticated = () => {
         if (new Date() > new Date(isAuthenticated?.expires_at)) {
             dispatch(logout())
         }
-        // console.log(isAuthenticated?.expires_at)
     })
 
 }
